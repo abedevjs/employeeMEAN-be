b6bad3251d6066a2dedaa99b01973e306ac7e544
Muhammad Akbar <bilarasa@gmail.com>
2022-12-25 17:57:01 +0700

    switch names

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors')
+// const mongoose = require('mongoose')
 
+//! Environment configuration --start
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+// console.log(process.env);
 
-// const { mongoose } = require('./db');
-const employeeController = require('./controllers/employeeController')
+process.on('uncaughtException', err => {//! Handling bugs/error in any asyncronous code. Declared before we start our Express app
+    console.log(err.name, err.message);
+    process.exit(2);
+});
 
-const app = express();
-app.use(bodyParser.json());
-app.use(cors({
-    origin: '*'
-}));
+const app = require('./main');
+console.log(`App running on: ${app.get('env')}`);
+//! Environment configuration --end
 
-// app.listen(3000, () => console.log('Server running on port: 3000'))
+// mongoose.connect('mongodb://localhost:27017/FreeCodeCampMEAN', (err) => {
+//     if (!err) console.log('db connection done');
+//     else console.log('db connection fail:' + JSON.stringify(err, undefined, 2));
+// });
 
-app.use('/employees', employeeController)
+//! Database setup --start
+const mongoose = require('mongoose');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// mongoose.connect(process.env.DATABASE_LOCAL, {
+mongoose.connect(DB, {
+    // useNewUrlParser: true, //tdk usah krn sdh pake mongoose v6
+    // useCreateIndex: true, //tdk usah krn sdh pake mongoose v6
+    // useFindAndModify: false //tdk usah krn sdh pake mongoose v6
+}).then((data) => console.log(`Database is connected to: ${data.connection.host}`));
+//! Database setup --end
 
-module.exports = app
+//! Server --start
+const port = process.env.PORT || 8000;
+server = app.listen(port, () => {
+    console.log(`port ${port}`);
+});
+//! Server --start
+
+module.exports = mongoose;
